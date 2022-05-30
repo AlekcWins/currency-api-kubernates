1c891a3921c1c7bad8829092f0291c623059af93
Alexandr <aleksand2000@gmail.com>
2022-05-30 16:28:43 +0400

    deploy : add first deploy configs

---
+++ kubernates/active-mq/active-mq.bat
@@ -0,0 +1,3 @@
+kubectl apply -f ./active-mq-configmap.yml
+kubectl apply -f ./active-mq-credentials.yml
+kubectl apply -f ./active-mq-deployment.yml
---
+++ kubernates/postgres/postgres.bat
@@ -0,0 +1,3 @@
+kubectl apply -f ./postgres-configmap.yml
+kubectl apply -f ./postgres-credentials.yml
+kubectl apply -f ./postgres-deployment.yml
